6a2cb70068492b1cdf8e6e2ed75a63c0e86cd110
clixzy1337 <peristerasgiwrgos@gmail.com>
2024-11-26 12:51:58 -0800

    Add files via upload
    so with this batch file when you open it your windows pc theme changes to dark

---
+++ changetodark.bat
@@ -0,0 +1,2 @@
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
